27c6fa199774e483c422616676a468d73e9a9466
Belks53 <sigalbella35@gmail.com>
2025-11-18 15:23:35 +0300

    create new file

--- lab12/src/Main.java
+++ lab12/src/Main.java
@@ -1,9 +1,16 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        File file = new File("C:\\Users\\Belka1\\Desktop\\");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("input.txt");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+
+        }
     }
 }
